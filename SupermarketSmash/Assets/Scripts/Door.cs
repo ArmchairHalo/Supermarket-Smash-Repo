07bb6835942504db04ed6997a3e772955e099358
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2023-05-25 22:49:41 +0100

    added comments to the door script so its easy to understand whats happening

--- SupermarketSmash/Assets/Scripts/Door.cs
+++ SupermarketSmash/Assets/Scripts/Door.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    //string so that you are able to type in what scene is to be changed
     public string levelToLoad;
 
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log("Object hit the door");
 
+        //if statement is used so that only the object with the Player tag
+        //can trigger the scene change
         if (other.tag == "Player")
         {
+            //changes the scene to whatever has been assigned by the designer
             SceneManager.LoadScene(levelToLoad);
         }
 

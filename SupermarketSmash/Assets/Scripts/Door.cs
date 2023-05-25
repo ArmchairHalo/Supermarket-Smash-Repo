74e6177cd58fc6b0946b62c634c9a9728f5e9e90
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2023-05-25 22:40:03 +0100

    gotten the door script to work now i need to make it so that the score unlocks the door

--- SupermarketSmash/Assets/Scripts/Door.cs
+++ SupermarketSmash/Assets/Scripts/Door.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    public string levelToLoad;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Object hit the door");
+
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        
+    }
 }

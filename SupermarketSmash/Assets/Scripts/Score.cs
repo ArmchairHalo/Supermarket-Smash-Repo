047bdad7430d46e3f97b6f2101754a33c6758683
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2023-05-26 19:45:14 +0100

    made it so that the score does not reset when the scene changes

--- SupermarketSmash/Assets/Scripts/Score.cs
+++ SupermarketSmash/Assets/Scripts/Score.cs
@@ -7,10 +7,24 @@ public class Score : MonoBehaviour
 {
     //public variable
     public Text scoreDisplay;
+    public bool shouldReset = false;
 
     //Private variables
-    private int scoreValue = 0;
+    private static int scoreValue = 0;
 
+    //Called by Unity the frame after this object is created
+    void Start()
+    {
+        //Check if we should be reseting the score this scene
+        if (shouldReset == true)
+        {
+            //Reset the score value back to 0
+            scoreValue = 0;
+        }
+
+        //Update the display of the score based on the numerical value
+        scoreDisplay.text = scoreValue.ToString();
+    }
     //Function to add to the player's score
     //Not automatically called by unity - we need to call it ourselves in our code
     public void AddScore(int toAdd)
